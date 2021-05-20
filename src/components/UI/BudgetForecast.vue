52ac1f1f9d409e737669bf6b727997cdcbf9fc65
Tyler D <tyler@tdaigle.com>
2021-05-20 16:02:18 -0400

    charts not working

--- src/components/UI/BudgetForecast.vue
+++ src/components/UI/BudgetForecast.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="budget-forecast-container">
-    <h2>Budget Forecast</h2>
+    <!-- <h2>Budget Forecast</h2>
     <p>There are {{ numTransactions }} items in the list</p>
     <p>The Starting value is {{ startingValue }}</p>
     <ul>
       <li v-for="(amount, idx) in forecast" :key="idx">
         {{ idx }}: {{ amount }}
       </li>
-    </ul>
+    </ul> -->
+    <forecast-chart :forecastData="forecast" />
   </div>
 </template>
 
 <script>
 import { forecastBudget } from "../../utils/calculate";
+import ForecastChart from "./ForecastChart";
 
 export default {
+  components: {
+    ForecastChart,
+  },
   props: {
     transactionList: {
       type: Object,
